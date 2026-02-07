d862e200668253fefa3ebd7b109d21d566f8da56
kkkkiyojj <p24037473@gmail.com>
2026-02-07 21:01:46 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import MiniCalendarWidget from "../components/MiniCalendarWidget";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center p-8">
+    // 노션 임베드용: 화면 꽉 채우는 레이아웃(min-h-screen) 금지
+    <main className="p-0 m-0">
       <MiniCalendarWidget />
     </main>
   );
