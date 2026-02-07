f31f8ae0323644391b72f75246cd2f657a37e4d2
kkkkiyojj <p24037473@gmail.com>
2026-02-07 19:50:19 +0900

    Add MiniCalendarWidget to Home component

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import MiniCalendarWidget from "@/components/MiniCalendarWidget";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center p-8">
+      <MiniCalendarWidget />
+    </main>
+  );
+}
